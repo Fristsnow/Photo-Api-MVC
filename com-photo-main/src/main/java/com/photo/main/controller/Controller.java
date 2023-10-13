439ba9b2afcdcbe77e8f4d4507a240655d1cc4f1
Firstsnow Lucky <firstsnow1119@163.com>
2023-10-13 17:31:30 +0800

    Changes

---
+++ com-photo-main/src/main/java/com/photo/main/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.photo.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String list(){
+        return "hello world";
+    }
+}
